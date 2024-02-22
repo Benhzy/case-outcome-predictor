9d49f3c625e6755835c4161cd4941b447f052aa9
Benhzy <benhzy98@gmail.com>
2024-02-22 09:46:08 +0800

    modified s3_helper.py
    create the data/raw dir automatically, and modified upload & download to use client instead as bucket is made private

--- s3_helper.py
+++ s3_helper.py
@@ -5,17 +5,22 @@ AWS_REGION = os.environ.get("AWS_REGION")
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-client = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID,
+s3 = boto3.client('s3', region_name=AWS_REGION, aws_access_key_id=AWS_ACCESS_KEY_ID,
                           aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-s3 = boto3.resource("s3")
-bucket = s3.Bucket('sg-auth-bucket')
+bucket = 'sg-auth-bucket'
+os.makedirs('data/raw', exist_ok=True)
 
 def case_upload():
     folder_path = 'raw-cases'
     for filename in os.listdir(folder_path):
-        bucket.download_file(filename, f"{folder_path}/{filename}")
+        s3.download_file(bucket, filename, f"{folder_path}/{filename}")
 
 def case_download():
-    objects = s3.list_objects_v2(Bucket=bucket)
+    objects = s3.list_objects_v2(Bucket = bucket)
     for obj in objects['Contents']:
-        bucket.download_file(obj['Key'], f"raw-cases/{obj['Key']}")
+        s3.download_file(bucket, obj['Key'], f"data/raw/{obj['Key']}")
+
+
+# Uncomment the following lines to upload to/download from s3
+#case_upload()
+#case_download()
