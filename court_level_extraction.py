731d1947c575744b1776f98f92489bae69f3289d
e0727131 <e0727131@u.nus.edu>
2024-03-15 15:44:47 +0800

    Completed Tribunal/Court Level Extraction

---
+++ court_level_extraction.py
@@ -0,0 +1,14 @@
+import os
+
+PATH = r"data\raw"
+
+def extract_court_level(path=PATH) -> set:
+    """ Extract the court level for each legal case found in the given folder path """
+
+    court_levels = set()
+    for file in os.listdir(path):
+        year, court_level, case = file.split("_")
+        if court_level not in court_levels:
+            court_levels.add(court_level)
+    return court_levels
+# print(extract_court_level(PATH))
